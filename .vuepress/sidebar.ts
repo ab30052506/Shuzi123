7b27f36599fa9beb90823de8bfa5e485b950a1b9
ab30052506 <ab30052506@users.noreply.github.com>
2026-04-03 13:21:39 +0800

    fix: 修复侧边栏路径大小写问题，添加member目录自动侧边栏

--- .vuepress/sidebar.ts
+++ .vuepress/sidebar.ts
@@ -3,7 +3,9 @@ import AI from "./sidebars/ai";
 // @ts-ignore
 export default {
   "/AI/": AI,
+  "/ai/": AI,
   "/AI项目教程/": AI,
+  "/member/": "auto",
   // 降级，默认根据文章标题渲染侧边栏
   "/": "auto",
 } as SidebarConfig4Multiple;
